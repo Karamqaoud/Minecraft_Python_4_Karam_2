5786b38c96d51769effce5ccadc4fc66939d6cfd
[Deubank671] <[Deubank671@lsr7.net]>
2018-01-23 11:06:14 -0600

    new

--- functions/woolColor.py
+++ functions/woolColor.py
@@ -1,19 +1,39 @@
 from mcpi.minecraft import Minecraft
-mc = Minecraft.craft()
+mc = Minecraft.create()
 
-block = 35
-state = 6
-mc.setBlocks(10, 3, -4, block, state)
-
-mc.setBlocks(11, 3, -4, 20, 6, -8, block, state)
 
 def getWoolState(color):
     if color == "pink":
         blockState = 6
-        elif:
+    elif color == "green":
+        blockState = 13
+    elif color == "light blue":
+        blockState = 3
+    elif color == "light gray":
+        blockState = 8
+    elif color == "lime":
+        blockState = 5
+    elif color == "orange":
+        blockState = 1
+    elif color == "red":
+        blockState = 14
+    elif color == "yellow":
+        blockState = 4
+    elif color == "black":
+        blockState = 15
+    elif color == "blue":
+        blockState = 11
+    elif color == "brown":
+        blockState = 12
+    elif color == "cyan":
+        blockState = 9
+    elif color == "gray":
+        blockState = 7
+    return blockState
             
-colorString = input("Enter a block color: ")
+colorString = str(input("Enter a block color: "))
 state = getWoolState(colorString)
 
+
 pos = mc.player.getTilePos()
-mc.setBlock(po.x, poy.y, pos.z, 35, state)
+mc.setBlock(pos.x, pos.y, pos.z, 35, state)
